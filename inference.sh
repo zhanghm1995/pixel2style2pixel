038ab443e5f2654def16c45844f7f6b69b485be0
Herman <zhanghm_1995@qq.com>
2022-05-26 10:27:58 +0800

    add gitignore and inference.sh and run inference ok

---
+++ inference.sh
@@ -0,0 +1,8 @@
+python scripts/inference.py \
+       --exp_dir=work_dir/demo_inference \
+       --checkpoint_path=/data/data0/zhanghm/Models/psp/psp_celebs_sketch_to_face.pt \
+       --data_path=work_dir/test \
+       --test_batch_size=4 \
+       --test_workers=4 \
+       --couple_outputs \
+       --latent_mask=8,9,10,11,12,13,14,15,16,17
